8ec3009f53d85349a60f424e15784a75f75b186a
clindsey4 <clindsey4@murraystate.edu>
2024-04-09 15:58:03 -0500

    Implement Territories.

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -17,10 +17,15 @@ extends Node3D
 	2, 2, 2, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0
 ]
 
-@export_group("Teams")
+@export_group("Materials")
 @export var light_material: Material
 @export var dark_material: Material
 @export var cathedral_material: Material
+@export var error_material: Material
+@export var hover_material: Material
+@export var light_territory_material: Material
+@export var dark_territory_material: Material
+@export var hover_territory_material: Material
 
 ######### Preload Pieces #########
 var piece_scenes: Array[PackedScene] = [
@@ -40,19 +45,34 @@ var piece_scenes: Array[PackedScene] = [
 	preload("res://objects/pieces/cathedral.tscn"), 	# Cathedral
 ]
 
+var _territory_marker = preload("res://objects/board_territory_marker.tscn")
+
 ######### Script Variables #########
 var selected_cell: Vector2
 
 # placing piece information
-var placing_piece_rotation: float
+var _placing_piece_rotation: float
 var _placing_piece_type: Globals.Piece
 var _placing_piece_min_bound: Vector2
 var _placing_piece_max_bound: Vector2
-var placing_piece: Piece
+var _placing_piece: Piece
 
-var board = [] # Array[Array[Tile]]
+var _board = [] # Array[Array[Tile]]
 var _placed_pieces: Array[Piece] = []
+var _territory_markers: Array[MeshInstance3D]
+var _hover_territory: int = -1 # the index of the territory that is currently being hovered over
+var _territories = [
+	[], # dark
+	[] # light
+]
+var _territories_tile_maps = [
+	{}, # dark
+	{} # light
+]
+
+# game info
 var _current_team: Globals.Team = Globals.Team.CATHEDRAL
+var _turn_count: int = 0
 ######### Children #########
 @onready var _board_node = $GridLines
 
@@ -67,16 +87,22 @@ func _get_piece_material(team: Globals.Team) -> Material:
 			return cathedral_material
 	return light_material
 
-func to_cell(pos: Vector3) -> Vector2:
+func to_cell(pos: Vector3):
 	var scale = _board_node.scale
 	
 	var cell_width = scale.x / grid_columns
 	var cell_height = scale.y / grid_rows
 	
-	var col = max(0, min(grid_columns - 1, floor(pos.x / cell_width)))
-	var row = max(0, min(grid_rows - 1, floor(pos.z / cell_height)))
+	var col = floor(pos.x / cell_width)
+	var row = floor(pos.z / cell_height)
 	
-	return Vector2(col, row)
+	var clamped_col = max(0, min(grid_columns - 1, col))
+	var clamped_row = max(0, min(grid_rows - 1, row))
+	
+	return [
+		Vector2(clamped_col, clamped_row),
+		col != clamped_col or row != clamped_row
+	]
 
 func world_to_local(pos: Vector3) -> Vector3:
 	var scale = _board_node.scale
@@ -84,8 +110,8 @@ func world_to_local(pos: Vector3) -> Vector3:
 	var min_pos = (_board_node.position - half_scale) * Vector3(1.0, 0.0, 1.0)
 	var max_pos = (_board_node.position + half_scale) * Vector3(1.0, 0.0, 1.0)
 	
-	var x = max(min_pos.x, min(max_pos.x, pos.x))
-	var z = max(min_pos.z, min(max_pos.z, pos.z))
+	var x = max(min_pos.x - 1, min(max_pos.x, pos.x))
+	var z = max(min_pos.z - 1, min(max_pos.z, pos.z))
 	
 	x = max_pos.x + x
 	z = max_pos.z + z
@@ -130,7 +156,7 @@ func _place_piece(type: Globals.Piece, team: Globals.Team, cell: Vector2, rotati
 	
 	# update tiles
 	for shape_offset: Vector2 in piece.get_shape_rotated(rotation):
-		board[cell.x + shape_offset.x][cell.y + shape_offset.y][2] = piece
+		_board[cell.x + shape_offset.x][cell.y + shape_offset.y][2] = piece
 	
 	# play animation
 	piece.animator.play("piece_animations/place_piece")
@@ -139,12 +165,12 @@ func _place_piece(type: Globals.Piece, team: Globals.Team, cell: Vector2, rotati
 	
 	return true
 
-func _refresh_piece_bounds(piece: Piece = placing_piece, rotation: float = 0.0) -> void:
+func _refresh_piece_bounds(piece: Piece = _placing_piece, piece_rotation: float = 0.0) -> void:
 	var min_x: int = 0
 	var min_y: int = 0
 	var max_x: int = 0
 	var max_y: int = 0
-	for offset: Vector2 in piece.get_shape_rotated(rotation):
+	for offset: Vector2 in piece.get_shape_rotated(piece_rotation):
 		min_x = min(min_x, offset.x)
 		min_y = min(min_y, offset.y)
 		max_x = max(max_x, offset.x)
@@ -155,7 +181,7 @@ func _refresh_piece_bounds(piece: Piece = placing_piece, rotation: float = 0.0) 
 
 func _begin_piece_placement(type: Globals.Piece, team: Globals.Team) -> void:
 	# debounce
-	if placing_piece:
+	if _placing_piece:
 		return
 	
 	# set the type of the piece that we're placing
@@ -167,7 +193,7 @@ func _begin_piece_placement(type: Globals.Piece, team: Globals.Team) -> void:
 		return
 
 	# reset rotation to normal
-	placing_piece_rotation = 0.0
+	_placing_piece_rotation = 0.0
 	
 	# load the piece into the scene and set it as a child of the board
 	var piece: Piece = piece_scene.instantiate()
@@ -178,37 +204,45 @@ func _begin_piece_placement(type: Globals.Piece, team: Globals.Team) -> void:
 	_refresh_piece_bounds(piece)
 	
 	# set default position
-	_set_selected_cell(selected_cell, piece)
+	_set_selected_cell(selected_cell)
 	
 	# add to board
 	add_child(piece)
 	
 	# set the placing piece variable
-	placing_piece = piece
+	_placing_piece = piece
+	
+	# signal on piece placement signal
+	get_tree().call_group("board", "on_piece_placement_begin", type)
 	 
 func _end_piece_placement() -> bool:
-	if !placing_piece:
+	if !_placing_piece:
 		return false
 	
+	var type = _placing_piece.type
+	
 	var status = _place_piece(
-		placing_piece.type,
-		placing_piece.team,
+		type,
+		_placing_piece.team,
 		selected_cell,
-		placing_piece_rotation
+		_placing_piece_rotation
 	)
 	
 	# delete the scene
-	placing_piece.queue_free()
+	_placing_piece.queue_free()
+	
+	# call on end
+	get_tree().call_group("board", "on_piece_placement_end", type, status)
 	
 	# reset variables
-	placing_piece = null
+	_placing_piece = null
 	
 	return status
 	
 func _cancel_piece_placement() -> void:
-	if placing_piece:
-		placing_piece.queue_free()
-	placing_piece = null
+	if _placing_piece:
+		_placing_piece.queue_free()
+	_placing_piece = null
 # checks if a tile is valid
 func _is_tile_valid(pos: Vector2) -> bool:
 	# check if the tile is in bounds
@@ -218,36 +252,295 @@ func _is_tile_valid(pos: Vector2) -> bool:
 		return false
 		
 	# check if the tile is taken
-	var tile = board[x][y]
+	var tile = _board[x][y]
 	return tile[2] == null
 
 # checks if a tile for a piece is valid
-func _is_piece_tile_valid(pos: Vector2, piece: Piece, rotation: float) -> bool:
+func _is_piece_tile_valid(pos: Vector2, piece: Piece, piece_rotation: float) -> bool:
+	var opposing_team = Globals.Team.DARK if _current_team == Globals.Team.LIGHT else Globals.Team.LIGHT
+	var pos_hash = _hash_coord(pos.x, pos.y)
+	if _territories_tile_maps[opposing_team].has(pos_hash):
+		if _territories[opposing_team][_territories_tile_maps[opposing_team][pos_hash]][1]:
+			return false
+	
 	# process the tiles to check
-	for shape_offset: Vector2 in piece.get_shape_rotated(rotation):
+	for shape_offset: Vector2 in piece.get_shape_rotated(piece_rotation):
 		if !_is_tile_valid(pos + shape_offset):
 			return false
 	
 	return true
 	
-func _set_selected_cell(new_cell: Vector2, piece: Piece) -> void:
+func _set_selected_cell(new_cell: Vector2) -> void:
 	new_cell.x = clamp(new_cell.x, abs(_placing_piece_min_bound.x), (grid_columns - 1) - _placing_piece_max_bound.x)
 	new_cell.y = clamp(new_cell.y, abs(_placing_piece_min_bound.y), (grid_rows - 1) - _placing_piece_max_bound.y)
 	
 	selected_cell = new_cell
 
+func _hash_coord(x: int, y: int) -> String:
+	return str(x,'-',y)
+
+func _flood_fill(x: int, y: int, team: Globals.Team, visited: Dictionary):
+	if x < 0 or x >= grid_columns or y < 0 or y >= grid_rows:
+		return [true, []]
+		
+	var coord_hash = _hash_coord(x, y)
+	var tile = _board[x][y]
+	var tile_piece = tile[2]
+	
+	if (tile_piece != null and tile_piece.team == team) or visited.has(coord_hash):
+		return [true, []]
+		
+	visited[coord_hash] = true
+	var area = [Vector2(x, y)]
+	var enclosed = true
+	
+	var directions = [Vector2(0, -1), Vector2(1, 0), Vector2(0, 1), Vector2(-1, 0), Vector2(1, 1), Vector2(-1, -1), Vector2(1, -1), Vector2(-1, 1)]
+	for direction in directions:
+		var result = _flood_fill(x + direction.x, y + direction.y, team, visited)
+		enclosed = enclosed and result[0]
+		for new_tile in result[1]:
+			area.append(new_tile)
+	
+	return [enclosed, area]
+
+func _get_territories(team: Globals.Team):
+	var visited = {}
+	var territories = []
+	
+	for territory in _territories[team]:
+		# if territory is claimed, add it to the visited list
+		if territory[1]:
+			for tile in territory[0]:
+				visited[_hash_coord(tile.x, tile.y)] = true
+
+	for x in range(grid_columns):
+		for y in range(grid_rows):
+			if !visited.has(_hash_coord(x, y)):
+				var result = _flood_fill(x, y, team, visited)
+				if result[0] and result[1]:
+					territories.append(result[1])
+	
+	return territories
+
+func _render_territory(territory, material: Material) -> void:
+	for cell in territory[0]:
+		var marker = _territory_marker.instantiate()
+		marker.position = _get_cell_world_position(cell) + Vector3(0, 0.01, 0)
+		marker.set_surface_override_material(0, material)
+		add_child(marker)
+		_territory_markers.append(marker)
+
+func _refresh_render_territories() -> void:
+	# delete old territory markers
+	for marker in _territory_markers:
+		marker.queue_free()
+	
+	_territory_markers = []
+	
+	# render the hover territory
+	if _hover_territory != -1 and _territories[_current_team][_hover_territory][1] != true:
+		_render_territory(_territories[_current_team][_hover_territory], hover_territory_material)
+	
+	var team_n = 0
+	for team_territories in _territories:
+		for territory in team_territories:
+			if territory[1]:
+				_render_territory(territory, dark_territory_material if team_n == Globals.Team.DARK else light_territory_material)
+		team_n += 1
+
+func _get_team_valid_territories(team: Globals.Team):
+	var unchecked_territories = _get_territories(team)
+	var valid_territories = []
+	for territory in unchecked_territories:
+		if _is_territory_valid(territory, team):
+			valid_territories.append([
+				territory, # tiles
+				false, # claimed
+			])
+	return valid_territories
+	
+func _get_territories_tile_map(territories) -> Dictionary:
+	var tile_map = {}
+	var n = 0
+	for territory in territories:
+		for tile in territory[0]:
+			tile_map[_hash_coord(tile.x, tile.y)] = n
+		n += 1
+	return tile_map
+	
+# checks if any given territory is valid for a given team
+func _is_territory_valid(territory, team: Globals.Team) -> bool:
+	var pieces = {}
+	var opposing_piece_count = 0
+
+	for cell in territory:
+		var tile = _board[cell.x][cell.y]
+		var tile_piece = tile[2]
+		if tile_piece and !pieces.has(tile_piece):
+			if tile_piece.team != team:
+				opposing_piece_count += 1
+			pieces[tile_piece] = true
+	
+	return 1 >= opposing_piece_count
+
+func _claim_territory(territory_index: int):
+	var territory = _territories[_current_team][territory_index]
+	if !territory[1]:
+		# remove piece inside of the territory
+		var piece = null
+		for cell in territory[0]:
+			var tile = _board[cell.x][cell.y]
+			if tile[2]:
+				if !piece:
+					piece = tile[2]
+				tile[2] = null
+				
+		if piece:
+			match(piece.team):
+				Globals.Team.LIGHT:
+					light_piece_counts[piece.type] = light_piece_counts[piece.type] + 1
+				Globals.Team.DARK:
+					dark_piece_counts[piece.type] = dark_piece_counts[piece.type] + 1
+			piece.queue_free()
+			
+		territory[1] = true
+		
+func _update_team_territories(team: Globals.Team):
+	var new_territories = []
+	for territory in _territories[team]:
+		if territory[1]:
+			new_territories.append(territory)
+
+	var valid = _get_team_valid_territories(team)
+	for territory in valid:
+		new_territories.append(territory)
+
+	_territories[team] = new_territories
+	_territories_tile_maps[team] = _get_territories_tile_map(new_territories)
+
 ######### Board Group Signals #########
 func set_mouse_position(new_pos: Vector3) -> void:
-	if placing_piece:
-		_set_selected_cell(to_cell(world_to_local(new_pos)), placing_piece)
+	var to_cell_result = to_cell(world_to_local(new_pos))
+	var hover_cell = to_cell_result[0]
+	if _placing_piece:
+		_set_selected_cell(hover_cell)
+
+	# handle the hovering piece
+	if _turn_count >= 3:
+		var tile_map = _territories_tile_maps[_current_team]
+		var cell_hash = _hash_coord(hover_cell.x, hover_cell.y)
+		var has_hash = tile_map.has(cell_hash)
+
+		if has_hash and _hover_territory != tile_map[cell_hash] and !to_cell_result[1] and !_placing_piece:
+			_hover_territory = tile_map[cell_hash]
+			for cell in _territories[_current_team][_hover_territory][0]:
+				var tile = _board[cell.x][cell.y]
+				var piece = tile[2]
+				if piece and is_instance_valid(piece):
+					piece.material = hover_material
+					break
+			_refresh_render_territories()
+		elif (!has_hash or to_cell_result[1] or _placing_piece) and _hover_territory != -1:
+			_hover_territory = -1
+			for cell in _territories[_current_team][_hover_territory][0]:
+				var tile = _board[cell.x][cell.y]
+				var piece = tile[2]
+				if piece and is_instance_valid(piece):
+					piece.material = _get_piece_material(piece.team)
+					break
+			_refresh_render_territories()
+		
+#func on_piece_placement_begin(piece: Globals.Piece) -> void:
+#	return
+
+# called when piece placement ends
+func on_piece_placement_end(piece: Globals.Piece, success: bool) -> void:
+	if !_placing_piece:
+		return
+		
+	if success:
+		match(_current_team):
+			Globals.Team.CATHEDRAL:
+				light_piece_counts[Globals.Piece.CATHEDRAL] = light_piece_counts[Globals.Piece.CATHEDRAL] - 1
+				_current_team = Globals.Team.DARK
+			Globals.Team.LIGHT:
+				light_piece_counts[piece] = light_piece_counts[piece] - 1
+				_current_team = Globals.Team.DARK
+			Globals.Team.DARK:
+				dark_piece_counts[piece] = dark_piece_counts[piece] - 1
+				_current_team = Globals.Team.LIGHT
+		
+		_turn_count += 1
+		
+		# dark team can only claim territory after turn 3 (their second turn)
+		if _turn_count >= 3:
+			_update_team_territories(Globals.Team.DARK)
+		
+		# light team can only claim territory after turn 4 (their second turn)
+		if _turn_count >= 4:
+			_update_team_territories(Globals.Team.LIGHT)
+		
+		# reset the hover territory
+		_hover_territory = -1
+		_refresh_render_territories()
+		
 func hud_begin_piece_placement(piece: Globals.Piece) -> void:
 	_cancel_piece_placement()
-	_begin_piece_placement(piece, _current_team)
+	match(_current_team):
+		Globals.Team.CATHEDRAL:
+			var remaining = light_piece_counts[Globals.Piece.CATHEDRAL]
+			if remaining > 0:
+				_begin_piece_placement(Globals.Piece.CATHEDRAL, _current_team)
+		Globals.Team.LIGHT:
+			match(piece):
+				Globals.Piece.DARK_ABBEY:
+					piece = Globals.Piece.LIGHT_ABBEY
+				Globals.Piece.DARK_ACADEMY:
+					piece = Globals.Piece.LIGHT_ACADEMY
+					
+			var remaining = light_piece_counts[piece]
+			if remaining > 0:
+				_begin_piece_placement(piece, _current_team)
+		Globals.Team.DARK:
+			match(piece):
+				Globals.Piece.LIGHT_ABBEY:
+					piece = Globals.Piece.DARK_ABBEY
+				Globals.Piece.LIGHT_ACADEMY:
+					piece = Globals.Piece.DARK_ACADEMY
+					
+			var remaining = dark_piece_counts[piece]
+			if remaining > 0:
+				_begin_piece_placement(piece, _current_team)
+
+func hud_end_piece_placement() -> void:
+	_end_piece_placement()
+
+func hud_cancel_piece_placement() -> void:
+	_cancel_piece_placement()
+
+func hud_on_skip_turn_pressed() -> void:
+	# don't skip until after turn 4
+	if _turn_count > 4:
+		_cancel_piece_placement()
+		match(_current_team):
+			Globals.Team.LIGHT:
+				_current_team = Globals.Team.DARK
+			Globals.Team.DARK:
+				_current_team = Globals.Team.LIGHT
+		
+		_turn_count += 1
+	
 ######### Signals #########
 func _input(event):
 	if event is InputEventMouseButton:
 		if !event.is_canceled() and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			_end_piece_placement()
+			if _placing_piece:
+				_end_piece_placement()
+			elif _hover_territory != - 1:
+				# claim territory
+				_claim_territory(_hover_territory)
+				_hover_territory = -1
+				
 
 func _ready():
 	# initialize the board variable
@@ -259,7 +552,7 @@ func _ready():
 				row_n,
 				null
 			])
-		board.append(row);
+		_board.append(row);
 	
 	# default board set up
 	#_place_piece(Globals.Piece.INFIRMARY, Globals.Team.DARK, Vector2(5.0, 5.0), 0.0)
@@ -269,15 +562,23 @@ func _ready():
 	#_place_piece(Globals.Piece.CATHEDRAL, Globals.Team.CATHEDRAL, Vector2(1.0, 1.0), 90.0)
 	
 	# place piece
-	_begin_piece_placement(Globals.Piece.CATHEDRAL, Globals.Team.CATHEDRAL)
+	#_begin_piece_placement(Globals.Piece.CATHEDRAL, Globals.Team.CATHEDRAL)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if placing_piece:
+	if _placing_piece:
 		# update piece position
 		var target_pos = _get_cell_world_position(selected_cell)
-		placing_piece.position = placing_piece.position.lerp(target_pos, delta * 16)
-		placing_piece.rotation_degrees = placing_piece.rotation_degrees.lerp(Vector3(0.0, placing_piece_rotation, 0.0), delta * 16)
+		_placing_piece.position = _placing_piece.position.lerp(target_pos, delta * 16)
+		_placing_piece.rotation_degrees = _placing_piece.rotation_degrees.lerp(Vector3(0.0, _placing_piece_rotation, 0.0), delta * 16)
+		
+		# check tile validity
+		var material = _get_piece_material(_current_team)
+		if !_is_piece_tile_valid(selected_cell, _placing_piece, _placing_piece_rotation):
+			if _placing_piece.material != error_material:
+				_placing_piece.material = error_material
+		elif _placing_piece.material != material:
+			_placing_piece.material = material
 		
 		# check inputs
 		if Input.is_action_just_pressed("piece_left") or Input.is_action_just_pressed("piece_right") or Input.is_action_just_pressed("piece_up") or Input.is_action_just_pressed("piece_down"):
@@ -288,11 +589,11 @@ func _process(delta):
 			
 			input = input.rotated(Vector3.UP, camera.rotation.y).normalized()
 
-			_set_selected_cell(Vector2(clamp(round(selected_cell.x + input.x), 0, 9), clamp(round(selected_cell.y + input.z), 0, 9)), placing_piece)
+			_set_selected_cell(Vector2(clamp(round(selected_cell.x + input.x), 0, 9), clamp(round(selected_cell.y + input.z), 0, 9)))
 		if Input.is_action_just_pressed("piece_rotate"):
-			placing_piece_rotation += 90.0
-			_refresh_piece_bounds(placing_piece, placing_piece_rotation)
-			_set_selected_cell(selected_cell, placing_piece)
+			_placing_piece_rotation += 90.0
+			_refresh_piece_bounds(_placing_piece, _placing_piece_rotation)
+			_set_selected_cell(selected_cell)
 		if Input.is_action_just_pressed("piece_place"):
 			_end_piece_placement()
 		if Input.is_action_just_pressed("cancel_placement"):
--- scripts/Hud.gd
+++ scripts/Hud.gd
@@ -1,23 +1,70 @@
 extends Control
 
 ######### Variables #########
-var is_ready: bool = false
-var default_toggle_text: String
+var _is_ready: bool = false
+var _default_toggle_text: String
+
+var _clicking_piece: bool = false
+var _dragging_piece: bool = false
+var _click_piece_type: Globals.Piece
+var _click_piece_start_mouse_pos: Vector2
 
 ######### Children #########
 @onready var toggle_camera_button = $FlowContainer/Toggle
 
+######### Functions #########
+func _begin_click(piece_type: Globals.Piece) -> void:
+	# debounce
+	if _clicking_piece:
+		return
+	
+	_clicking_piece = true
+	_click_piece_type = piece_type
+	
+	# set start mouse position
+	var viewport = get_viewport()
+	_click_piece_start_mouse_pos = viewport.get_mouse_position()
+	
+func _end_click() -> void:
+	if !_clicking_piece:
+		return
+		
+	if _dragging_piece:
+		var viewport = get_viewport()
+		var mouse_pos = viewport.get_mouse_position()
+		if (_click_piece_start_mouse_pos - mouse_pos).x >= 200:
+			get_tree().call_group("board", "hud_end_piece_placement")
+		else:
+			get_tree().call_group("board", "hud_cancel_piece_placement")
+		_dragging_piece = false
+	else:
+		get_tree().call_group("board", "hud_begin_piece_placement", _click_piece_type)
+		
+	_clicking_piece = false
+
 ######### Camera Group Signals #########
 func on_camera_mode_changed(new_mode: bool) -> void:
-	default_toggle_text = "Top Down" if new_mode else "Isometric"
-	if is_ready:
-		toggle_camera_button.text = default_toggle_text
+	_default_toggle_text = "Top Down" if new_mode else "Isometric"
+	if _is_ready:
+		toggle_camera_button.text = _default_toggle_text
 
 ######### Signals #########
+func _input(event):
+	if event is InputEventMouseMotion and _clicking_piece and !_dragging_piece:
+		var viewport = get_viewport()
+		var mouse_pos = viewport.get_mouse_position()
+		
+		var diff = _click_piece_start_mouse_pos - mouse_pos
+		if diff.x >= 200 and !_dragging_piece:
+			#start dragging
+			_dragging_piece = true
+			get_tree().call_group("board", "hud_begin_piece_placement", _click_piece_type)
+		
+
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-	is_ready = true
-	toggle_camera_button.text = default_toggle_text
+	_is_ready = true
+	toggle_camera_button.text = _default_toggle_text
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -35,7 +82,102 @@ func _on_right_pressed():
 func _on_toggle_pressed():
 	get_tree().call_group("hud", "on_toggle_camera_mode")
 
+func _on_skip_pressed():
+	get_tree().call_group("hud", "hud_on_skip_turn_pressed")
 
-func _on_tavern_pressed():
-	get_tree().call_group("board", "hud_begin_piece_placement", Globals.Piece.TAVERN)
+######### Piece Button Signals #########
+func _on_tavern_button_down():
+	_begin_click(Globals.Piece.TAVERN)
+	pass
+
+func _on_tavern_button_up():
+	_end_click()
+	pass
+
+func _on_stable_button_down():
+	_begin_click(Globals.Piece.STABLE)
 	pass # Replace with function body.
+	
+func _on_stable_button_up():
+	_end_click()
+	pass
+
+func _on_inn_button_down():
+	_begin_click(Globals.Piece.INN)
+	pass # Replace with function body.
+
+func _on_inn_button_up():
+	_end_click()
+	pass
+
+func _on_bridge_button_down():
+	_begin_click(Globals.Piece.BRIDGE)
+	pass # Replace with function body.
+
+func _on_bridge_button_up():
+	_end_click()
+	pass
+
+func _on_square_button_down():
+	_begin_click(Globals.Piece.SQUARE)
+	pass # Replace with function body.
+
+func _on_square_button_up():
+	_end_click()
+	pass
+
+func _on_manor_button_down():
+	_begin_click(Globals.Piece.MANOR)
+	pass # Replace with function body.
+
+func _on_manor_button_up():
+	_end_click()
+	pass
+
+func _on_abbey_button_down():
+	_begin_click(Globals.Piece.LIGHT_ABBEY)
+	pass # Replace with function body.
+
+func _on_abbey_button_up():
+	_end_click()
+	pass
+
+func _on_academy_button_down():
+	_begin_click(Globals.Piece.LIGHT_ACADEMY)
+	pass # Replace with function body.
+
+func _on_academy_button_up():
+	_end_click()
+	pass
+
+func _on_infirmary_button_down():
+	_begin_click(Globals.Piece.INFIRMARY)
+	pass # Replace with function body.
+
+func _on_infirmary_button_up():
+	_end_click()
+	pass
+
+func _on_castle_button_down():
+	_begin_click(Globals.Piece.CASTLE)
+	pass # Replace with function body.
+
+func _on_castle_button_up():
+	_end_click()
+	pass
+	
+func _on_tower_button_down():
+	_begin_click(Globals.Piece.TOWER)
+	pass # Replace with function body.
+
+func _on_tower_button_up():
+	_end_click()
+	pass
+
+func _on_cathedral_button_down():
+	_begin_click(Globals.Piece.CATHEDRAL)
+	pass # Replace with function body.
+
+func _on_cathedral_button_up():
+	_end_click()
+	pass
